42667734527d93aa24870bfb951dcfabac54c362
carlos <kalelcarlosmartinez@gmail.com>
2022-02-10 22:05:46 -0800

    26 tutorial done

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 //import logo from './logo.svg';
 import './App.css';
 import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/search-box.component';
 
 class App extends Component {
 
@@ -9,6 +10,7 @@ class App extends Component {
     super();
     this.state = {
       monsters: [],
+      searchField : ''
     };
   }
 
@@ -19,10 +21,18 @@ class App extends Component {
   }
 
   render(){
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster => 
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
+
     return (
       <div className="App">
-      <CardList monsters={this.state.monsters}>
-      </CardList>
+      <SearchBox
+        placeholder='search monsters'
+        handleChange={e => this.setState({ searchField : e.target.value })}
+      />
+      <CardList monsters={ filteredMonsters } />
       </div>
     )
   }
